d73806dbaf88e78d0cd8afa74909239c8db19bfd
mquig42 <mquig42@users.noreply.github.com>
2024-12-01 00:27:04 -0500

    Test of basic functionality. Read file from SD card and print to screen.

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
 #include <Arduino.h>
+#include <M5Cardputer.h>
+#include <SPI.h>
+#include <SD.h>
 
-// put function declarations here:
-int myFunction(int, int);
+//Pins for SD card
+#define SD_SPI_SCK_PIN  40
+#define SD_SPI_MISO_PIN 39
+#define SD_SPI_MOSI_PIN 14
+#define SD_SPI_CS_PIN   12
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup()
+{
+	auto cfg = M5.config();
+	M5Cardputer.begin(cfg);
+	M5Cardputer.Display.drawString("Advent of Code 2024", 0, 10);
+
+	//init SD card
+	SPI.begin(SD_SPI_SCK_PIN, SD_SPI_MISO_PIN, SD_SPI_MOSI_PIN, SD_SPI_CS_PIN);
+	SD.begin(SD_SPI_CS_PIN, SPI, 25000000);
+
+	File file = SD.open("/AoC_Data/1_Test.txt");
+	M5Cardputer.Display.drawString(file.readStringUntil('\n'), 0, 20);
+	file.close();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
