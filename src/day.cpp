c47d29cadb8c5feafe26151d12386f53da023a21
mquig42 <mquig42@users.noreply.github.com>
2024-12-07 20:39:25 -0500

    Fixed indentation

--- src/day.cpp
+++ src/day.cpp
@@ -6,6 +6,6 @@ void Day::solveBoth(LovyanGFX* display)
     display->setTextFont(2);
 
     display->printf("Advent of Code 2024 Day %d\n", dayNumber);
-	display->printf("Part 1: %llu\n", solve1());
+    display->printf("Part 1: %llu\n", solve1());
     display->printf("Part 2: %llu\n", solve2());
 }
