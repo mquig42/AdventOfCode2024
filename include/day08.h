f11b589aad841cd077e98eda29ebad25de8138ee
mquig42 <mquig42@users.noreply.github.com>
2024-12-09 14:06:13 -0500

    Day 8 both parts

--- include/day08.h
+++ include/day08.h
@@ -1,6 +1,7 @@
 #ifndef DAY08_H
 #define DAY08_H
 #include <unordered_map>
+#include <unordered_set>
 #include "day.h"
 #include "utility.h"
 
@@ -11,10 +12,13 @@ class Day08 : public Day
         void load(File file);
     private:
         std::unordered_map<char, std::vector<uint16_t>> antennas;
+        std::unordered_set<uint16_t> antinodes;
         uint8_t numRows, numCols;
         
         uint64_t solve1();
         uint64_t solve2();
+        uint8_t findAntinodes(uint16_t a1, uint16_t a2, uint8_t multiplier);
+        void findAntinodes2(uint16_t a1, uint16_t a2);
 };
 
 #endif //DAY08_H
