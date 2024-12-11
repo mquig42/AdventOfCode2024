15e40042ec118c9036dbb28300917a6a67b71304
mquig42 <mquig42@users.noreply.github.com>
2024-12-11 14:44:29 -0500

    Added memoization

--- include/day11.h
+++ include/day11.h
@@ -1,6 +1,7 @@
 #ifndef DAY11_H
 #define DAY11_H
 #include <cmath>
+#include <unordered_map>
 #include "day.h"
 
 class Day11 : public Day
@@ -10,6 +11,7 @@ class Day11 : public Day
         void load(File file);
     private:
         std::vector<uint32_t> input;
+        std::unordered_map<uint64_t, uint32_t> memo;
 
         uint64_t solve1();
         uint64_t solve2();
--- src/day11.cpp
+++ src/day11.cpp
@@ -28,13 +28,28 @@ uint64_t Day11::solve1()
 uint64_t Day11::solve2()
 {
     return 0;
+
+    uint64_t count = 0;
+    for(uint32_t n : input)
+    {
+        memo.clear();
+        count += blink(n, 75);
+    }
+    return count;
 }
 
 //Return number of stones after numBlinks, for one stone with starting value n
+//Part 1 without memoization is solved in 6 seconds. With memoization is instant
+//On part 2 it runs out of memory
 uint64_t Day11::blink(uint64_t n, uint8_t numBlinks)
 {
+    if(memo.count(n * 100 + numBlinks))
+        return memo[n * 100 + numBlinks];
+    
     int digits;
     int pow10;
+    uint64_t nOrig = n;
+    uint8_t numBlinksOrig = numBlinks;
     while(numBlinks > 0)
     {
         digits = std::log10(n) + 1;
@@ -46,8 +61,9 @@ uint64_t Day11::blink(uint64_t n, uint8_t numBlinks)
         else if(digits % 2 == 0)
         {
             pow10 = std::pow(10, digits / 2);
-            //M5Cardputer.Display.printf("%d has %d digits. Split to %d and %d\n", n, digits, n / pow10, n % pow10);
-            return blink(n / pow10, numBlinks - 1) + blink(n % pow10, numBlinks - 1);
+            uint64_t splitReturn = blink(n / pow10, numBlinks - 1) + blink(n % pow10, numBlinks - 1);
+            memo[nOrig * 100 + numBlinksOrig] = splitReturn;
+            return splitReturn;
         }
         else
         {
@@ -55,5 +71,7 @@ uint64_t Day11::blink(uint64_t n, uint8_t numBlinks)
             numBlinks--;
         }
     }
+
+    memo[nOrig * 100 + numBlinksOrig] = 1;
     return 1;
 }
