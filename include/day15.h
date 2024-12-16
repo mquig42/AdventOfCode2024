8a6db0002420af24fdc220674bc2731f15cfc205
mquig42 <mquig42@users.noreply.github.com>
2024-12-16 13:52:03 -0500

    Day 15 part 2

--- include/day15.h
+++ include/day15.h
@@ -12,6 +12,11 @@ class Day15 : public Day
         std::unordered_set<uint16_t> walls;
         std::unordered_set<uint16_t> boxes;
         uint16_t robot;
+
+        std::unordered_set<uint16_t> walls2;
+        std::unordered_set<uint16_t> boxes2; //Stores only the left side of each box
+        uint16_t robot2;
+
         String instructions;
         int8_t up = -100;
         int8_t right = 1;
@@ -21,6 +26,9 @@ class Day15 : public Day
         uint64_t solve1();
         uint64_t solve2();
         void moveRobot(char instr);
+        void moveRobot2(char instr);
+        bool canMoveBox2(uint16_t pos, int8_t direction);
+        void moveBox2(uint16_t pos, int8_t direction);
 };
 
 #endif //DAY15_H
--- src/day15.cpp
+++ src/day15.cpp
@@ -17,6 +17,7 @@ void Day15::load(File file)
         if(line.length() == 1)
             break;
         
+        //Make map for part 1
         for(int i = 0; i < line.length(); i++)
         {
             if(line[i] == '#')
@@ -25,6 +26,21 @@ void Day15::load(File file)
                 boxes.insert(row * 100 + i);
             else if(line[i] == '@')
                 robot = row * 100 + i;
+        }
+
+        //Make map for part 2
+        line.replace("#", "##");
+        line.replace(".", "..");
+        line.replace("O", "[]");
+        line.replace("@", "@.");
+        for(int i = 0; i < line.length(); i++)
+        {
+            if(line[i] == '#')
+                walls2.insert(row * 100 + i);
+            else if(line[i] == '[')
+                boxes2.insert(row * 100 + i);
+            else if(line[i] == '@')
+                robot2 = row * 100 + i;
         }
         row++;
     }
@@ -47,12 +63,27 @@ uint64_t Day15::solve1()
         sum += b;
     }
 
+    //These are no longer needed. Might as well free some memory.
+    walls.clear();
+    boxes.clear();
+
     return sum;
 }
 
 uint64_t Day15::solve2()
 {
-    return 0;
+    for(char i : instructions)
+    {
+        moveRobot2(i);
+    }
+
+    uint64_t sum = 0;
+    for(uint16_t b : boxes2)
+    {
+        sum += b;
+    }
+
+    return sum;
 }
 
 void Day15::moveRobot(char instr)
@@ -96,4 +127,143 @@ void Day15::moveRobot(char instr)
         //Empty space. Just move
         robot += direction;
     }
+}
+
+void Day15::moveRobot2(char instr)
+{
+    int8_t direction;
+    if(instr == '^')
+        direction = up;
+    else if(instr == '>')
+        direction = right;
+    else if(instr == 'v')
+        direction = down;
+    else if(instr == '<')
+        direction = left;
+    else
+        return;
+    
+    //Positions of any box the robot could push depends on direction
+    if(direction == left)
+    {
+        if(boxes2.count(robot2 + 2 * direction))
+        {
+            if(canMoveBox2(robot2 + 2 * direction, direction))
+            {
+                moveBox2(robot2 + 2 * direction, direction);
+                robot2 += direction;
+            }
+        }
+        else if(!walls2.count(robot2 + direction))
+        {
+            robot2 += direction;
+        }
+    }
+    else if(direction == right)
+    {
+        if(boxes2.count(robot2 + direction))
+        {
+            if(canMoveBox2(robot2 + direction, direction))
+            {
+                moveBox2(robot2 + direction, direction);
+                robot2 += direction;
+            }
+        }
+        else if(!walls2.count(robot2 + direction))
+        {
+            robot2 += direction;
+        }
+    }
+    else
+    {
+        //When moving up or down, there are two places a box could be
+        uint16_t box = 0;
+        if(boxes2.count(robot2 + direction - 1))
+            box = robot2 + direction - 1;
+        else if(boxes2.count(robot2 + direction))
+            box = robot2 + direction;
+
+        if(box != 0)
+        {
+            if(canMoveBox2(box, direction))
+            {
+                moveBox2(box, direction);
+                robot2 += direction;
+            }
+        }
+        else if(!walls2.count(robot2 + direction))
+        {
+            robot2 += direction;
+        }
+    }
+}
+
+//Given a direction and the position of the LEFT side of a box,
+//Can the box be moved?
+bool Day15::canMoveBox2(uint16_t pos, int8_t direction)
+{
+    if(direction == left || direction == right)
+    {
+        //Iterative
+        uint16_t space = pos + 2 * direction;
+        while(boxes2.count(space))
+            space += 2 * direction;
+        
+        //Boxes are two spaces wide, but we're only moving one.
+        //So, if direction is left, we might have skipped over an empty space
+        if(direction == left)
+            space += 1;
+        
+        return !walls2.count(space);
+    }
+    else
+    {
+        //Check for walls
+        if(walls2.count(pos + direction) || walls2.count(pos + direction + 1))
+            return false;
+        
+        //Check for box directly above/below this one
+        //If there is, then we only need one recursive call
+        if(boxes2.count(pos + direction))
+            return canMoveBox2(pos + direction, direction);
+        
+        //Check for offset boxes. There may be two of them
+        bool lBox = true;
+        bool rBox = true;
+        if(boxes2.count(pos + direction - 1))
+            lBox = canMoveBox2(pos + direction - 1, direction);
+        if(boxes2.count(pos + direction + 1))
+            rBox = canMoveBox2(pos + direction + 1, direction);
+        
+        return(lBox && rBox);
+    }
+}
+
+//Move a box, plus any other boxes it pushes
+//Use canMoveBox2 first to verify, this does not check
+void Day15::moveBox2(uint16_t pos, int8_t direction)
+{
+    //Move any boxes this one pushes first
+    if(direction == left || direction == right)
+    {
+        //For left and right, there can be only one
+        if(boxes2.count(pos + 2 * direction))
+        {
+            moveBox2(pos + 2 * direction, direction);
+        }
+    }
+    else
+    {
+        //For up and down, there could be a pushed box in 3 different positions
+        if(boxes2.count(pos + direction - 1))
+            moveBox2(pos + direction - 1, direction);
+        if(boxes2.count(pos + direction))
+            moveBox2(pos + direction, direction);
+        if(boxes2.count(pos + direction + 1))
+            moveBox2(pos + direction + 1, direction);
+    }
+
+    //Now move this box
+    boxes2.erase(pos);
+    boxes2.insert(pos + direction);
 }
