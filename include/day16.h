7ac44e0ddf054f731c3516ee0fa8bc6a3e7f0f6d
mquig42 <mquig42@users.noreply.github.com>
2024-12-16 21:12:51 -0500

    Day 16 part 1

--- include/day16.h
+++ include/day16.h
@@ -1,6 +1,8 @@
 #ifndef DAY16_H
 #define DAY16_H
 #include <unordered_set>
+#include <unordered_map>
+#include <queue>
 #include "day.h"
 #include "utility.h"
 
@@ -10,8 +12,43 @@ class Day16 : public Day
         Day16();
         void load(File file);
     private:
+        struct state
+        {
+            uint16_t position;
+            uint8_t direction;
+
+            bool operator==(state s) const
+            {
+                return this->position == s.position && this->direction == s.direction;
+            }
+
+            size_t operator()(state s) const
+            {
+                return s.position;
+            }
+        };
+
+        struct queueNode
+        {
+            uint16_t position;
+            uint8_t direction;
+            uint32_t score;
+            bool operator<(const queueNode &b) const
+            {
+                return score > b.score; //Easiest way to make queue give minimum value instead of maximum
+            }
+        };
+
+        uint8_t up = 0;
+        uint8_t right = 1;
+        uint8_t down = 2;
+        uint8_t left = 3;
+
         std::unordered_set<uint16_t> spaces;
         uint16_t start, end;
+
+        std::unordered_map<state, uint32_t, state> visited;
+        std::priority_queue<queueNode> unvisited;
 
         uint64_t solve1();
         uint64_t solve2();
--- src/day16.cpp
+++ src/day16.cpp
@@ -29,14 +29,68 @@ void Day16::load(File file)
                 spaces.insert(makeCoord(row, i));
             }
         }
+
+        row++;
     }
 
     file.close();
 }
 
 //I'm not sure there's enough memory to do Dijkstra's algorithm on a 141x141 grid with direction as part of state
+//There isn't. It works for the sample though.
 uint64_t Day16::solve1()
 {
+    uint16_t nextPos;
+    uint8_t nextDir;
+    queueNode current;
+
+    visited[{start, 1}] = 0;
+    unvisited.push({start, 1, 0});
+
+    while(!unvisited.empty())
+    {
+        current = unvisited.top();
+        unvisited.pop();
+
+        if(current.position == end)
+            return current.score;
+        
+        //From any state there are 3 possible moves:
+        //Rotate left, rotate right, and go forward
+        //nextPos represents the position directly in front
+        if(current.direction == up)
+            nextPos = makeCoord(row(current.position) - 1, col(current.position));
+        else if(current.direction == right)
+            nextPos = makeCoord(row(current.position), col(current.position) + 1);
+        else if(current.direction == down)
+            nextPos = makeCoord(row(current.position) + 1, col(current.position));
+        else
+            nextPos = makeCoord(row(current.position), col(current.position) - 1);
+        
+        //If nextPos is a valid space and hasn't already been reached by a shorter path, add it to unvisited
+        if(spaces.count(nextPos) && (visited.count({nextPos, current.direction}) == 0 || visited[{nextPos, current.direction}] > current.score + 1))
+        {
+            unvisited.push({nextPos, current.direction, current.score + 1});
+            visited[{nextPos, current.direction}] = current.score + 1;
+        }
+        
+        //Turn right
+        nextDir = (current.direction + 1) % 4;
+        if(visited.count({current.position, nextDir}) == 0 || visited[{current.position, nextDir}] > current.score + 1000)
+        {
+            unvisited.push({current.position, nextDir, current.score + 1000});
+            visited[{current.position, nextDir}] = current.score + 1000;
+        }
+
+        //Turn left
+        nextDir = (current.direction + 3) % 4;
+        if(visited.count({current.position, nextDir}) == 0 || visited[{current.position, nextDir}] > current.score + 1000)
+        {
+            unvisited.push({current.position, nextDir, current.score + 1000});
+            visited[{current.position, nextDir}] = current.score + 1000;
+        }
+    }
+
     return 0;
 }
 
