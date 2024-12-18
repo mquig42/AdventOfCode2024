3232883fc20c833f5157916fa1953f327b60bfbe
mquig42 <mquig42@users.noreply.github.com>
2024-12-18 14:41:05 -0500

    Day 18 part 1

--- include/day18.h
+++ include/day18.h
@@ -11,6 +11,16 @@ class Day18 : public Day
         Day18();
         void load(File file);
     private:
+        struct queueNode
+        {
+            uint16_t location;
+            uint32_t steps;
+        };
+
+        std::vector<uint16_t> bytes;
+        uint8_t maxDim = 70; //The space is square, so this is maxRow and maxCol
+        uint16_t toDrop = 1024;
+
         uint64_t solve1();
         uint64_t solve2();
 };
--- src/day18.cpp
+++ src/day18.cpp
@@ -7,11 +7,71 @@ Day18::Day18()
 
 void Day18::load(File file)
 {
+    while(file.available())
+    {
+        bytes.push_back(makeCoord(file.parseInt(), file.parseInt()));
+    }
+    bytes.pop_back();
+
+    //Detect sample input and use smaller map
+    if(bytes.size() < 1024)
+    {
+        maxDim = 6;
+        toDrop = 12;
+    }
+
     file.close();
 }
 
+//Drop some bytes, then do a bread first search
 uint64_t Day18::solve1()
 {
+    std::unordered_set<uint16_t> corrupted;
+    std::unordered_set<uint16_t> visited;
+    std::queue<queueNode> q;
+    queueNode current;
+    uint16_t next;
+
+    for(int i = 0; i < toDrop; i++)
+    {
+        corrupted.insert(bytes[i]);
+    }
+
+    visited.insert(0);
+    q.push({0, 0});
+    while(!q.empty())
+    {
+        current = q.front();
+        q.pop();
+
+        if(row(current.location) == maxDim && col(current.location) == maxDim)
+            return current.steps;
+
+        next = makeNeighbour(current.location, 'U');
+        if(row(next) <= maxDim && col(next) <= maxDim && !corrupted.count(next) && !visited.count(next))
+        {
+            visited.insert(next);
+            q.push({next, current.steps + 1});
+        }
+        next = makeNeighbour(current.location, 'R');
+        if(row(next) <= maxDim && col(next) <= maxDim && !corrupted.count(next) && !visited.count(next))
+        {
+            visited.insert(next);
+            q.push({next, current.steps + 1});
+        }
+        next = makeNeighbour(current.location, 'D');
+        if(row(next) <= maxDim && col(next) <= maxDim && !corrupted.count(next) && !visited.count(next))
+        {
+            visited.insert(next);
+            q.push({next, current.steps + 1});
+        }
+        next = makeNeighbour(current.location, 'L');
+        if(row(next) <= maxDim && col(next) <= maxDim && !corrupted.count(next) && !visited.count(next))
+        {
+            visited.insert(next);
+            q.push({next, current.steps + 1});
+        }
+    }
     return 0;
 }
 
