7935d036cd424d6e85564750b7acb4077fdfc605
mquig42 <mquig42@users.noreply.github.com>
2024-12-19 17:15:37 -0500

    Day 19 part 1

--- include/day19.h
+++ include/day19.h
@@ -1,5 +1,6 @@
 #ifndef DAY19_H
 #define DAY19_H
+#include <unordered_map>
 #include "day.h"
 
 class Day19 : public Day
@@ -8,8 +9,14 @@ class Day19 : public Day
         Day19();
         void load(File file);
     private:
+        std::vector<String> towels;
+        std::vector<String> patterns;
+        std::unordered_map<uint8_t, bool> memo;
+        
         uint64_t solve1();
         uint64_t solve2();
+        bool subStrCmp(String str, String sub, uint8_t startIdx);
+        bool match1(String pattern, uint8_t startIdx);
 };
 
 #endif //DAY19_H
--- src/day19.cpp
+++ src/day19.cpp
@@ -7,15 +7,86 @@ Day19::Day19()
 
 void Day19::load(File file)
 {
+    //First challenge: We need something like String.split() to separate all those towels
+    String firstLine = file.readStringUntil('\n');
+    int nextComma;
+    int i = 0;
+    while(i < firstLine.lastIndexOf(','))
+    {
+        nextComma = firstLine.indexOf(',', i);
+        towels.push_back(firstLine.substring(i, nextComma));
+        i = nextComma + 2;
+    }
+    towels.push_back(firstLine.substring(i, firstLine.indexOf('\r')));
+
+    //empty line between towels and patterns
+    file.readStringUntil('\n');
+
+    while(file.available())
+    {
+        //Read only the pattern, not the newline chars
+        patterns.push_back(file.readStringUntil('\r'));
+        file.readStringUntil('\n');
+    }
+
     file.close();
 }
 
 uint64_t Day19::solve1()
 {
-    return 0;
+    uint32_t sum = 0;
+    for(String p : patterns)
+    {
+        memo.clear();
+        if(match1(p, 0))
+        {
+            sum++;
+        }
+    }
+
+    return sum;
 }
 
 uint64_t Day19::solve2()
 {
     return 0;
+}
+
+bool Day19::subStrCmp(String str, String sub, uint8_t startIdx)
+{
+    for(uint8_t i = 0; i < sub.length(); i++)
+    {
+        if(i + startIdx >= str.length() || str[i + startIdx] != sub[i])
+            return false;
+    }
+    return true;
+}
+
+bool Day19::match1(String pattern, uint8_t startIdx)
+{
+    if(memo.count(startIdx))
+        return memo[startIdx];
+    
+    if(startIdx >= pattern.length())
+    {
+        memo[startIdx] = true;
+        return true;
+    }
+    
+    bool m;
+    for(String t : towels)
+    {
+        if(subStrCmp(pattern, t, startIdx))
+        {
+            m = match1(pattern, startIdx + t.length());
+            if(m)
+            {
+                memo[startIdx] = true;
+                return true;
+            }
+        }
+    }
+
+    memo[startIdx] = false;
+    return false;
 }
