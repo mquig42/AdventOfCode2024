b36371b471a68221d1844e334b258db8d33f29a6
mquig42 <mquig42@users.noreply.github.com>
2024-12-23 17:43:34 -0500

    Day 23 part 2

--- include/day23.h
+++ include/day23.h
@@ -10,12 +10,14 @@ class Day23 : public Day
     public:
         Day23();
         void load(File file);
+        void solveBoth(LovyanGFX* display);
     private:
         std::unordered_map<uint16_t, std::unordered_set<uint16_t>> connections;
-        std::vector<uint16_t> computers;
+        std::unordered_set<uint16_t> biggestGroup;
 
         uint64_t solve1();
         uint64_t solve2();
+        void bronKerbosch(std::unordered_set<uint16_t> &R, std::unordered_set<uint16_t> &P, std::unordered_set<uint16_t> &X);
 };
 
 #endif //DAY23_H
--- src/day23.cpp
+++ src/day23.cpp
@@ -10,7 +10,6 @@ void Day23::load(File file)
     //Going to store the computers as coords to avoid using too many Strings
     String line;
     uint16_t a, b;
-    bool containsA, containsB;
 
     while(file.available())
     {
@@ -20,29 +19,29 @@ void Day23::load(File file)
 
         connections[a].insert(b);
         connections[b].insert(a);
-
-        containsA = false;
-        containsB = false;
-        for(uint16_t c : computers)
-        {
-            if(c == a)
-                containsA = true;
-            if(c == b)
-                containsB = true;
-        }
-
-        if(!containsA)
-            computers.push_back(a);
-        if(!containsB)
-            computers.push_back(b);
     }
     file.close();
+}
+
+//solve2() does its own printing, since the answer is not an integer.
+void Day23::solveBoth(LovyanGFX* display)
+{
+    display->setTextColor(GREEN);
+    display->setTextFont(2);
+
+    display->printf("Advent of Code 2024 Day %d\n", dayNumber);
+    display->printf("Part 1: %llu\n", solve1());
+    solve2();
 }
 
 //What a mess, but at least it works
 //slow though. Maybe find a different way to iterate through all the computers that only considers connected ones?
 uint64_t Day23::solve1()
 {
+    std::vector<uint16_t> computers;
+    for(auto i : connections)
+        computers.push_back(i.first);
+
     uint64_t count = 0;
     for(uint16_t i = 0; i + 2 < computers.size(); i++)
     {
@@ -67,5 +66,55 @@ uint64_t Day23::solve1()
 
 uint64_t Day23::solve2()
 {
+    std::unordered_set<uint16_t> R;
+    std::unordered_set<uint16_t> P;
+    std::unordered_set<uint16_t> X;
+
+    for(auto i : connections)
+        P.insert(i.first);
+    
+    bronKerbosch(R, P, X);
+
+    std::vector<uint16_t> lanComputers;
+    for(uint16_t i : biggestGroup)
+        lanComputers.push_back(i);
+    
+    std::sort(lanComputers.begin(), lanComputers.end());
+
+    M5Cardputer.Display.printf("Part 2: ");
+    for(uint16_t i : lanComputers)
+        M5Cardputer.Display.printf("%c%c ", row(i), col(i));
+    M5Cardputer.Display.printf("\n");
+
     return 0;
+}
+
+void Day23::bronKerbosch(std::unordered_set<uint16_t> &R, std::unordered_set<uint16_t> &P, std::unordered_set<uint16_t> &X)
+{
+    if(P.empty() && X.empty())
+    {
+        if(biggestGroup.size() < R.size())
+            biggestGroup = R;
+    }
+    while(!P.empty())
+    {
+        uint16_t v = *P.begin();
+        std::unordered_set<uint16_t> newR = R;
+        newR.insert(v);
+        std::unordered_set<uint16_t> newP;
+        for(uint16_t i : connections[v])
+        {
+            if(P.count(i))
+                newP.insert(i);
+        }
+        std::unordered_set<uint16_t> newX;
+        for(uint16_t i : connections[v])
+        {
+            if(X.count(i))
+                newX.insert(i);
+        }
+        bronKerbosch(newR, newP, newX);
+        P.erase(v);
+        X.insert(v);
+    }
 }
